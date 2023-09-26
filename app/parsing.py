b67a98eb7fbedf7b19c3cfbb6c419368c7abd0c2
Sylvester107 <91274489+Sylvester107@users.noreply.github.com>
2023-09-26 11:44:01 +0000

    successfully created passage_metadata.csv for all files

--- app/parsing.py
+++ app/parsing.py
@@ -7,6 +7,8 @@ import pandas as pd
 import numpy as np
 import json
 from pathlib import Path
+import glob
+import os
 
 #inspect the one dataset
 file_path='Data\Corpus\kwame-legal-EL-1680770407105_Technical.txt'
@@ -23,59 +25,78 @@ with open(file_path,'rt',encoding='UTF-8') as file:
 _Technical.txt contains __section__ which is the Judgement
 under each section is __paragraph__ 
 """
-## Extract the passages within paragraphs
+# Specify the folder path
+folder_path = "Data\Corpus"
 
-with open(file_path,'rt',encoding='UTF-8') as file:
-    #read file contents into a variable
-    file_contents_ls=file.readlines() #returnts a list
-    #remove the newline character using list comprehension
-    file_contents_ls=lines = [line.strip() for line in file_contents_ls]
-#print(file_contents_ls)
+# Use glob to select text files and their corresponding metadata JSON files
+file_pattern = os.path.join(folder_path, "*.txt")
+
+#to store processed passages and corresponding metadata
+all_passages=[]
+all_metadata=[]
+#initiate a for loop to iterate over files in corpus
+for text_file_path in glob.glob(file_pattern):
+
+    ## Extract the passages within paragraphs
+
+    with open(text_file_path,'rt',encoding='UTF-8') as file:
+        #read file contents into a variable
+        file_contents_ls=file.readlines() #returnts a list
+
+        #remove the newline character using list comprehension
+        file_contents_ls=lines = [line.strip() for line in file_contents_ls]
+    #print(file_contents_ls)
 
 
-#define a drop list
-drop_list=['__section__','__paragraph__']
+    #define a drop list
+    drop_list=['__section__','__paragraph__']
 
-filtered_list=filtered_list = [item for item in file_contents_ls if item not in drop_list]
-#print to see output
-#print(filtered_list)
+    filtered_list=filtered_list = [item for item in file_contents_ls if item not in drop_list]
+    #print to see output
 
-#combine all using the join function
-#form 5 sentence each to form a passage
+    #print(filtered_list)
+
+    
+    #form 5 sentence each to form a passage
 
 
-# Define the number of consecutive strings to combine
-group_size = 5
+    # Define the number of consecutive strings to combine
+    group_size = 5
 
-# Initialize an empty list to store the combined passages
-passages = []
+    # Initialize an empty list to store the combined passages
+    passages = []
 
-#for loop with a step to efficiently combine strings
-for i in range(0, len(filtered_list), group_size):
-    passage = ' '.join(filtered_list[i:i + group_size])
-    passages.append(passage)
+    #for loop with a step to efficiently combine strings
+    for i in range(0, len(filtered_list), group_size):
+        passage = ' '.join(filtered_list[i:i + group_size])
+        passages.append(passage)
 
-#combined passages Uncomment to check output 
-#for passage in passages:
-    #print(passage)
+    #combined passages Uncomment to check output 
+    #for passage in passages:
+        #print(passage)
 
-#join to form 1 passage
-passage_1=' '.join(passages)
-#print(passage_1)
+    #join to form 1 passage
+    processed_passage=' '.join(passages)
+    all_passages.append(processed_passage)
+    #print(passage_1)
 
-##Extract metadata
-metadata_path='Data\Corpus\kwame-legal-EL-1680770407105_Metadata.json'
-with open(metadata_path,'r',encoding='UTF-8') as f:
-    #read file contents into a variable
-    metadata=json.load(f)
-#print(metadata)
+    ##Extract metadata
+    metadata_file_path = text_file_path.replace("Technical", "Metadata").replace(".txt", ".json")
+    with open(metadata_file_path,'r',encoding='UTF-8') as f:
+        #read file contents into a variable
+        metadata=json.load(f)
+    #print(metadata)
 
+    all_metadata.append(metadata)
+
+#Create a CSV file
 passage_metadata={
-    'Passages':[passage_1],
-    'Metadata':[metadata]
+    'Passages':all_passages,
+    'Metadata':all_metadata
 }
-filepath = "app\pairings\passage_metadata.csv"
-df=pd.DataFrame(passage_metadata)
-df.to_csv(filepath, index=False)
+print(f'{all_passages[4]}\n\n\n,{all_metadata[4]}')
+#filepath = "app\pairings\passage_metadata.csv"
+#df=pd.DataFrame(passage_metadata)
+#df.to_csv(filepath, index=False)
 
-print(f"CSV file '{filepath}' has been created.")
+#print(f"CSV file '{filepath}' has been created.")
