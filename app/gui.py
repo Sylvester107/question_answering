04f4a6085d00d201c4853df7fa0bd5670f0f383f
Sylvester107 <91274489+Sylvester107@users.noreply.github.com>
2023-09-30 02:59:58 +0000

    complete gui

--- app/gui.py
+++ app/gui.py
@@ -1,19 +1,24 @@
 import streamlit as st
 import requests
-import json
+from huggingface_hub import InferenceClient
+import matplotlib.pyplot as plt
 #Set the page configs
 st.set_page_config(page_title='Question Answering bot',page_icon='😎',layout='centered')
-
+#image=plt.imread('app\img\gavel.jpg')
 #set the title
-st.title("Question Answering System")
+st.title("Question Answering System for Legal Practioners ⚖⚖")
+#st.image(image,caption='Youngest lawyer in Ghana',width=400,use_column_width=False)
+
 with st.form(key='question'):
     question=st.text_area(label='Type your question here',placeholder="What is an offer")
     button=st.form_submit_button("submit")
 col1,col2=st.columns([3,2])
+col1.write('<h2 style="font-size: 24px;"> Search Response from indexed documents </h2>',unsafe_allow_html=True)
+col2.write('<h2 style="font-size: 24px;"> large language model response </h2>',unsafe_allow_html=True)
 with col1:
     if button:
 
-        """Search Response from indexed documents"""
+       
         #url
         url="http://localhost:5000/ask"
         #  request body
@@ -45,3 +50,17 @@ with col1:
         print(response)
     else:
         st.warning('No question submited')
+
+with col2:
+    if button:
+
+        client = InferenceClient(token='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')
+        #prompt template(alpaca template)
+        prompt_template = f"""You are legal practitioner with indepth knowledge about law. write a response that correcty answers the following question
+
+        ### question:
+        {question}
+        ### Response: """
+        response_llm=client.text_generation(prompt=prompt_template,max_new_tokens=1000,model='tiiuae/falcon-7b-instruct')
+        st.write(response_llm)
+        print(response_llm)
